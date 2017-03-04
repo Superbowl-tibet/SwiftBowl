f7f3379ada25fe0a9076d1ae7824066f5ae7fdee
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-03-04 12:50:58 +0900

    全員分のPlayground Pageを追加

---
+++ Playground.playground/Pages/Example.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport
+
+// CocoaPodsでインストールしたframeworkもimportできる
+// もしimportエラーが起きたら、そのframeworkを選択して適当なsimulatorでビルドする
+import Spring
+
+var str = "Hello, playground"
+
+print(Spring.self)  // importできてる
+
+// 参考：[Xcode 7のPlaygroundで出来ることまとめ](http://qiita.com/mono0926/items/9f8324637d1dca1e7075)
+
+let redView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 200))
+redView.backgroundColor = UIColor.red
+
+PlaygroundPage.current.liveView = redView
+// Assistant EditorのTimelineでプレビューがでます
