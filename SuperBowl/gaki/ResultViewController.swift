3eca00c642847661648ac859af501658cae6a76f
MMizogaki <voice.hito@gmail.com>
2017-03-04 17:08:40 +0900

    Add
    ResultViewController.swift

---
+++ SuperBowl/gaki/ResultViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ResultViewController.swift
+//  SuperBowl
+//
+//  Created by MMizogaki on 2017/03/04.
+//  Copyright © 2017年 MMizogaki. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
