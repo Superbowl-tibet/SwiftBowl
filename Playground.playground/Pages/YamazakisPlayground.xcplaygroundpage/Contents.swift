6fb0a836e08196a16f8a1fb87dece6389d975c6d
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-03-20 15:20:30 +0900

    Measure touched points

--- Playground.playground/Pages/YamazakisPlayground.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/YamazakisPlayground.xcplaygroundpage/Contents.swift
@@ -8,16 +8,63 @@ var str = "Hello, playground"
 
 class DetectionView: UIView {
   
+  struct TouchPoint {
+    let origin: CGPoint
+    let time: Date
+    let velocity: CGFloat
+    //    let circleCenterPoint: CGPoint? // TODO: 実装する
+  }
+  
+  // MARK: - Accessor
+  private var touchPoints: [TouchPoint] = []
+  
   override var canBecomeFirstResponder: Bool {
     return true
   }
   
+  override var isMultipleTouchEnabled: Bool {
+    set {}
+    get {
+      return false
+    }
+  }
+  
+  // MARK: - Initializers
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    self.setup()
+  }
+  
+  required override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.setup()
+  }
+  
+  private func setup() {
+    super.isMultipleTouchEnabled = false
+  }
+  
+  // MARK: -
+  private func addTouch(touch: UITouch) {
+    // TODO: 古いtouchPointを削除する
+    
+    let touchPointInView = touch.location(in: self)
+    print(touchPointInView)
+  }
+  
+  // MARK: - UIResponder
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    print(#function)
+    guard let touch = touches.first else {
+      return
+    }
+    self.addTouch(touch: touch)
   }
   
   override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-    print(#function)
+    guard let touch = touches.first else {
+      return
+    }
+    self.addTouch(touch: touch)
   }
 }
 
