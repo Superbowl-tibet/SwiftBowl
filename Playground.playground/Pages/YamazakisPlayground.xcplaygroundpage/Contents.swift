96ec4cfea4e32dcd4992f79b97e38519d160aa4a
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-03-20 15:01:26 +0900

    Can become FirstResponder and can receive touch on the Playground

--- Playground.playground/Pages/YamazakisPlayground.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/YamazakisPlayground.xcplaygroundpage/Contents.swift
@@ -1,8 +1,31 @@
 //: [Previous](@previous)
 
 import Foundation
+import UIKit
 import PlaygroundSupport
 
 var str = "Hello, playground"
+
+class DetectionView: UIView {
+  
+  override var canBecomeFirstResponder: Bool {
+    return true
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    print(#function)
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    print(#function)
+  }
+}
+
+let redView = DetectionView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
+redView.backgroundColor = UIColor.red
+
+PlaygroundPage.current.liveView = redView
+print(redView.becomeFirstResponder())
+
 
 //: [Next](@next)
