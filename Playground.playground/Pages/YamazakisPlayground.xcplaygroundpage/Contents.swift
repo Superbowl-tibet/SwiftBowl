c813643db4938f9c5dccc2250222eab870a1afb6
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-03-20 18:02:59 +0900

    Calculate circle center and radius

--- Playground.playground/Pages/YamazakisPlayground.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/YamazakisPlayground.xcplaygroundpage/Contents.swift
@@ -20,17 +20,49 @@ extension CGPoint {
   }
 }
 
+func calculateCircleCenter(p1: CGPoint, p2: CGPoint, p3: CGPoint) -> CGPoint? {
+  
+  let m0 = p2.y * p1.x
+      - p1.y * p2.x
+      + p3.y * p2.x
+      - p2.y * p3.x
+      + p1.y * p3.x
+      - p3.y * p1.x
+  
+  let x = ((pow(p1.x, 2) + pow(p1.y, 2)) * (p2.y - p3.y)
+    + (pow(p2.x, 2) + pow(p2.y, 2)) * (p3.y - p1.y)
+    + (pow(p3.x, 2) + pow(p3.y, 2)) * (p1.y - p2.y))
+    / (m0 * 2.0)
+  
+  let y = 0
+    - ((pow(p1.x, 2) + pow(p1.y, 2)) * (p2.x - p3.x)
+    + (pow(p2.x, 2) + pow(p2.y, 2)) * (p3.x - p1.x)
+    + (pow(p3.x, 2) + pow(p3.y, 2)) * (p1.x - p2.x))
+    / (m0 * 2.0)
+  
+  if (x.isNaN == false) && (y.isNaN == false) {
+    return CGPoint(x: x, y: y)
+  }
+  return nil
+}
+
 class DetectionView: UIView {
   
   struct TouchPoint {
     let location: CGPoint
     let tappedAt: Date
     let velocity: CGFloat
-    //    let circleCenterPoint: CGPoint? // TODO: 実装する
+    let circleCenterPoint: CGPoint?
+    let radius: CGFloat?
   }
   
   // MARK: - Accessor
   private var touchPoints: [TouchPoint] = []
+  private let centerIndicator = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 10.0, height: 10.0))
+  private let centerIndicator2 = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 10.0, height: 10.0))
+  private let centerIndicator3 = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 4.0, height: 4.0))
+  private let centerIndicator4 = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 4.0, height: 4.0))
+  private let centerIndicator5 = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 4.0, height: 4.0))
   
   override var canBecomeFirstResponder: Bool {
     return true
@@ -56,6 +88,34 @@ class DetectionView: UIView {
   
   private func setup() {
     super.isMultipleTouchEnabled = false
+    
+    func setupIndicator(indicator: UIView) {
+
+      indicator.clipsToBounds = true
+      indicator.layer.cornerRadius = indicator.frame.size.width / 2.0
+      indicator.isUserInteractionEnabled = false
+      
+      self.addSubview(indicator)
+      indicator.center = self.center
+    }
+    
+    self.centerIndicator.backgroundColor = UIColor.white
+    self.centerIndicator.isHidden = true
+    setupIndicator(indicator: self.centerIndicator)
+    
+    self.centerIndicator2.backgroundColor = UIColor.clear
+    self.centerIndicator2.layer.borderColor = UIColor.white.cgColor
+    self.centerIndicator2.layer.borderWidth = 1.0
+    setupIndicator(indicator: self.centerIndicator2)
+    
+    self.centerIndicator3.backgroundColor = UIColor.white
+    setupIndicator(indicator: self.centerIndicator3)
+
+    self.centerIndicator4.backgroundColor = UIColor.white
+    setupIndicator(indicator: self.centerIndicator4)
+
+    self.centerIndicator5.backgroundColor = UIColor.white
+    setupIndicator(indicator: self.centerIndicator5)
   }
   
   // MARK: -
@@ -65,23 +125,49 @@ class DetectionView: UIView {
     let touchPointInView = touch.location(in: self)
     let now = Date()
     
-    let currentPoint: TouchPoint
+    let p3: TouchPoint
     
-    if let previousPoint = self.touchPoints.last {
+    let interval = 10
+    let p2Index = self.touchPoints.count - interval
+    let p1Index = self.touchPoints.count - (interval * 2)
+    
+    if p2Index >= 0 {
       
-      let interval = now.timeIntervalSince(previousPoint.tappedAt)
-      let distance = (touchPointInView - previousPoint.location).length
+      let p2 = self.touchPoints[p2Index]
+      let interval = now.timeIntervalSince(p2.tappedAt)
+      let distance = (touchPointInView - p2.location).length
       let velocity = distance / CGFloat(interval)
+      var centerPoint: CGPoint? = nil
+      var radius: CGFloat? = nil
       
-      currentPoint = TouchPoint(location: touchPointInView, tappedAt: now, velocity: velocity)
+      if p1Index >= 0 {
+        let p1 = self.touchPoints[p1Index]
+        
+        if let c = calculateCircleCenter(p1: p1.location, p2: p2.location, p3: touchPointInView) {
+          
+          radius = sqrt(pow(c.x-p1.location.x,2)+pow(c.y-p1.location.y,2))
+
+          self.centerIndicator2.frame.size = CGSize(width: radius! * 2.0, height: radius! * 2.0)
+          self.centerIndicator2.layer.cornerRadius = radius!
+          self.centerIndicator2.center = c
+          
+          centerPoint = c
+
+          
+          self.centerIndicator3.center = p1.location
+          self.centerIndicator4.center = p2.location
+          self.centerIndicator5.center = touchPointInView
+        }
+      }
+      
+      p3 = TouchPoint(location: touchPointInView, tappedAt: now, velocity: velocity, circleCenterPoint: centerPoint, radius: radius)
       
     } else {
-      
-      currentPoint = TouchPoint(location: touchPointInView, tappedAt: now, velocity: 0.0)
+      p3 = TouchPoint(location: touchPointInView, tappedAt: now, velocity: 0.0, circleCenterPoint: nil, radius: nil)
     }
     
-    self.touchPoints.append(currentPoint)
-    print(currentPoint)
+    self.touchPoints.append(p3)
+    print(p3)
   }
   
   // MARK: - UIResponder
@@ -106,5 +192,7 @@ redView.backgroundColor = UIColor.red
 PlaygroundPage.current.liveView = redView
 print(redView.becomeFirstResponder())
 
+let a = calculateCircleCenter(p1: CGPoint(x: 1, y: 1), p2: CGPoint(x: 2, y: 1), p3: CGPoint(x: 1, y: 2))
+print(a)
 
 //: [Next](@next)
